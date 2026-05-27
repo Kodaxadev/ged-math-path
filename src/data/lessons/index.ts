a149f868a5f249abd95cec92840ed1d0e00728e9
Kodaxa <Justin.DavisWE@icloud.com>
2026-05-27 11:41:03 -0700

    Assemble study lesson catalog

---
+++ src/data/lessons/index.ts
@@ -0,0 +1,19 @@
+import { orientationLessons } from './orientation';
+import { percentLessons } from './percents';
+import { equationLessons } from './equations';
+import { ratioRateLessons } from './ratiosRates';
+import { fractionLessons } from './fractions';
+import { geometryLessons } from './geometry';
+import { advancedLessons } from './advanced';
+import { toolReadinessLessons } from './toolsReadiness';
+
+export const lessons = [
+  ...orientationLessons,
+  ...percentLessons,
+  ...equationLessons,
+  ...ratioRateLessons,
+  ...fractionLessons,
+  ...geometryLessons,
+  ...advancedLessons,
+  ...toolReadinessLessons,
+];
