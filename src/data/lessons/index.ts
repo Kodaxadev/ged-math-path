e6880c51ad29f9936d2b20e3708db4eab7a8ec5f
Kodaxa <Justin.DavisWE@icloud.com>
2026-05-27 11:55:52 -0700

    Register complete GED lesson catalog

--- src/data/lessons/index.ts
+++ src/data/lessons/index.ts
@@ -1,19 +1,23 @@
 import { orientationLessons } from './orientation';
+import { foundationLessons } from './foundation';
 import { percentLessons } from './percents';
 import { equationLessons } from './equations';
 import { ratioRateLessons } from './ratiosRates';
 import { fractionLessons } from './fractions';
 import { geometryLessons } from './geometry';
 import { advancedLessons } from './advanced';
+import { coverageLessons } from './coverage';
 import { toolReadinessLessons } from './toolsReadiness';
 
 export const lessons = [
   ...orientationLessons,
+  ...foundationLessons,
   ...percentLessons,
   ...equationLessons,
   ...ratioRateLessons,
   ...fractionLessons,
   ...geometryLessons,
   ...advancedLessons,
+  ...coverageLessons,
   ...toolReadinessLessons,
 ];
